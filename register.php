81b9a71c9c5b0142817c85764c63d9de10971024
faiz92000 <111262408+faiz92000@users.noreply.github.com>
2023-08-12 21:43:51 +0200

    Create register.php

---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inscription</title>
+</head>
+<body>
+    <h2>Inscription</h2>
+    <form method="post" action="registration_process.php">
+        <label for="username">Nom d'utilisateur:</label>
+        <input type="text" id="username" name="username" required><br><br>
+        
+        <label for="password">Mot de passe:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        
+        <input type="submit" value="S'inscrire">
+    </form>
+</body>
+</html>
